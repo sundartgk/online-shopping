47c3dc6419b7ac121d17b45eeba7576f3a228a3a
sundartgk <srisundar89@gmail.com>
2018-04-04 00:03:24 +0530

    sample demo test succeeded

---
+++ onlineshop/src/main/java/net/project/onlineshop/controller/PageController.java
@@ -0,0 +1,41 @@
+package net.project.onlineshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = { "/", "/home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greetings", "Welcome to Spring Web MVC ");
+		return mv;
+	}
+
+	/*@RequestMapping(value = "/test")
+	public ModelAndView test(@RequestParam(value="greeting",required=false) String greeting) {
+		if(greeting==null){
+			greeting="Sundara rajan";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greetings", greeting);
+		return mv;
+	}
+	*/
+	
+	
+	@RequestMapping(value = "/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting") String greeting) {
+		if(greeting==null){
+			greeting="Sundara rajan";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greetings", greeting);
+		return mv;
+	}
+
+}
